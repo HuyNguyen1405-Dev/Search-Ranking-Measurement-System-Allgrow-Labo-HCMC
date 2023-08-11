0a3f3d35e3e73fc2c7032f880949d1b16118a809
NguyenVanHuy1405 <vanhuy141520@gmail.com>
2023-08-12 00:55:21 +0700

    create form search in content for project

--- client/src/components/content/Content.js
+++ client/src/components/content/Content.js
@@ -1,16 +1,88 @@
-import React from 'react'
-import './Content.css'
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useState, useEffect } from "react";
+import "./Content.css";
 
 const Content = () => {
-  return (
-    <div className='content'>
-        <img src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg"/>
-        <img src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg"/>
-        <img src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg"/>
-        <img src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg"/>
-        <img src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg"/>
-    </div>
-  )
-}
+  const initialValues = { url_link: "", keyword: "" };
+  const [formValues, setFormValues] = useState(initialValues);
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
 
-export default Content
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormErrors(validate(formValues));
+    console.log(formErrors);
+    setIsSubmit(true);
+  };
+
+  useEffect(() => {
+    if (Object.keys(formErrors).length === 0 && isSubmit) {
+      console.log(formValues);
+    }
+  }, formErrors);
+
+  const validate = (values) => {
+
+    const errors = {};
+    if (!values.url_link) {
+      errors.url_link = "Url link is required";
+    }
+    if (!values.keyword) {
+      errors.keyword = "Keyword is required";
+    } else if (values.keyword.length < 5) {
+      errors.keyword = "Password must be more than 5 characters";
+    }
+    return errors;
+
+  };
+
+  return (
+    <>
+      <div className="container">
+        <form onSubmit={handleSubmit}>
+          <h1 className="title_form">Search Ranking</h1>
+          <div className="ui divider"></div>
+          <div className="ui form">
+            <div className="url-link">
+              <label className="title_url">URL Link:</label>
+              <input
+                className="input_url"
+                type="text"
+                name="url_link"
+                placeholder="Url link"
+                value={formValues.url_link}
+                onChange={handleChange}
+              />
+            </div>
+            <p className="errors">{formErrors.url_link}</p>
+            <div className="keyword">
+              <label className="title_keyword">Keyword:</label>
+              <textarea
+                className="input_keyword"
+                type="text"
+                name="keyword"
+                placeholder="Keyword"
+                value={formValues.keyword}
+                onChange={handleChange}
+              />
+            </div>
+            <p>{formErrors.keyword}</p>
+            <button className="btn_submit">Submit</button>
+          </div>
+        </form>
+      </div>
+      <div className="container_results">
+        <div className="results">
+          <h1>g1</h1>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Content;
--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import './Header.css'
 import logo from '../../logo_agLabo.svg'
+import logoJapan from '../../jp.svg'
+import logoVietnam from '../../vn.svg'
 
 const Header = () => {
     return (
@@ -8,8 +10,11 @@ const Header = () => {
             <h1 className='logo_allgrow_labo'>
                 <img className='image_logo' src={logo} alt="Công ty TNHH Allgrow-labo" />
             </h1>
-            <h4 className='header-title'>Search Ranking Measurement System</h4>
-            <h4 className='country-work'>Search Ranking Measurement System</h4>
+            <h2 className='header-title'>Search Ranking Measurement System</h2>
+            <div className='country-work'>
+                <img className='Japan_logo' src={logoJapan} alt="Japan Country" />
+                <img className='VN_logo' src={logoVietnam} alt="VietNam Country" />
+            </div>
         </header>
     )
 }
